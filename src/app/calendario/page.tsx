a1224bfae9602065a336d211f937df2908c30a06
Flávia Monique <flaviamonique38@gmail.com>
2024-10-31 09:23:26 -0300

    Calendario de vacinas

---
+++ src/app/calendario/page.tsx
@@ -0,0 +1,93 @@
+import Header from "../componentes/Header/page";
+import Footer from "../componentes/Footer/page";
+import styles from "../calendario/calendario.module.css";
+
+
+export default function Calendario() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+
+      <div className={styles.tableContainer}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+                <th>Vacinas</th>
+                <th>Jan</th>
+                <th>Fev</th>
+                <th>Mar</th>
+                <th>Abr</th>
+                <th>Mai</th>
+                <th>Jun</th>       
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Vacina contra brucelose</td>
+              <td></td>
+              <td>x</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Vacina contra paratifo nos bezerros</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className={styles.tableContainer}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+                <th>Vacinas</th>
+                <th>Jul</th>
+                <th>Ago</th>
+                <th>Set</th>
+                <th>Out</th>
+                <th>Nov</th>
+                <th>Dez</th>   
+            </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>Vacina contra paratifo nas vacas prenhas</td>
+              <td>x</td>
+              <td>x</td>
+              <td>x</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Vacina contra paratifo nos bezerros</td>
+              <td></td>
+              <td>x</td>
+              <td>x</td>
+              <td>x</td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
